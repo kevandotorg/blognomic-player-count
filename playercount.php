70ecb599e6acbad5e25826062b8e12415af71045
Kevan Davis <kevan@kevan.org>
2020-12-21 15:12:17 +0000

    Add files via upload

---
+++ playercount.php
@@ -0,0 +1,38 @@
+<?
+
+$year = 2005;
+$month = 6;
+
+while ($year<2021)
+{
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://blognomic.com/$year/".sprintf('%02d', $month)."/");
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $archive = curl_exec($ch);
+        curl_close($ch);
+
+        preg_match_all("/posted by <a href=\"\/member\/\d+\">([^<]+)<\/a>/",$archive,$matches);
+
+        $players = array();
+        foreach($matches[1] as $name)
+        {
+                if(!in_array($name, $players))
+                { array_push($players,$name); }
+        }
+
+        natcasesort($players);
+
+        $row = "$year-$month,".sizeof($players).",\"";
+        foreach ($players as $player)
+        {
+                $row .= $player.", ";
+        }
+        $row = substr($row, 0, -2);
+
+        print $row."\"\n";
+        $month++;
+        if ($month==13) { $month = 1; $year++; }
+}
+
+?>
